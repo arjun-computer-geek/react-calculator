efaf7879bb6437568573a5cb5e41d4f123a5c86b
Arjun <arjun2000raj@gmail.com>
2020-08-19 02:19:17 +0530

    Updates

---
+++ precache-manifest.841bc8b865e46d5e143dca981996276b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93dcffac42fb5bcf3f21fee4e6243e19",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "855b73e171848fe6e438",
+    "url": "/react-calculator/static/css/main.7f9cd9b2.chunk.css"
+  },
+  {
+    "revision": "2c77b779130b82cfc468",
+    "url": "/react-calculator/static/js/2.f33c7e79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.f33c7e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855b73e171848fe6e438",
+    "url": "/react-calculator/static/js/main.0dbb8206.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
